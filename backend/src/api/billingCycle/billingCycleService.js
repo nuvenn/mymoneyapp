613d339b11e3f140bdf452fe55ee8f1f16a1d030
Daniel de Sales da Silva <ssales.daniel@gmail.com>
2018-07-31 17:58:44 -0300

    adicionando cors

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -1,32 +1,34 @@
 const BillingCycle = require('./billingCycle')
+const errorHandler = require('../common/errorHandler')
 
 BillingCycle.updateOptions({ new: true, runValidators: true })
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
+BillingCycle.after('post', errorHandler).after('put', errorHandler)
 
-BillingCycle.route('count', (req, res, next) =>{
+BillingCycle.route('count', (req, res, next) => {
     BillingCycle.count((error, value) => {
-        if(error){
+        if (error) {
             res.status(500).json({ errors: [error] })
         } else {
             res.json({ value })
-        }    
-    })    
+        }
+    })
 })
 
-BillingCycle.route('summary', (req, res, next) =>{
+BillingCycle.route('summary', (req, res, next) => {
     BillingCycle.aggregate([{
-       $project: { credit: {$sum: "$credits.value"}, debt: {$sum: "$debts.value"} }
+        $project: { credit: { $sum: "$credits.value" }, debt: { $sum: "$debts.value" } }
     }, {
-       $group: { _id: null, credit: {$sum: "$credit"}, debt: {$sum: "$debt"} } 
+        $group: { _id: null, credit: { $sum: "$credit" }, debt: { $sum: "$debt" } }
     }, {
-       $project: { _id: 0, credit: 1, debt: 1 }
-    }],(error, result) => {
-        if(error){
+        $project: { _id: 0, credit: 1, debt: 1 }
+    }], (error, result) => {
+        if (error) {
             res.status(500).json({ errors: [error] })
         } else {
-            res.json(result[0] || { credit:0, debt: 0 })
+            res.json(result[0] || { credit: 0, debt: 0 })
         }
-    })   
+    })
 })
 
 module.exports = BillingCycle
