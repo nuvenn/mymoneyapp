b4f05ccbd40a1c774189c4cb6774979cca932063
Daniel de Sales da Silva <ssales.daniel@gmail.com>
2018-07-31 16:50:09 -0300

    inseri aggregate

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 module.exports = function(server) {
     const router = express.Router()
-    server.use('api', router)
+    server.use('/api', router)
     const BillingCycle = require('../api/billingCycle/billingCycleService')
     BillingCycle.register(router, '/billingCycles')
 }
