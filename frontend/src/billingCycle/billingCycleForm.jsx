eef9ff5c85dfd055298e51db5735f523e7957165
Daniel de Sales da Silva <ssales.daniel@gmail.com>
2018-08-22 15:42:45 -0300

    adicionei readOnly

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -8,14 +8,14 @@ import Input from '../common/form/labelAndInput'
 
 class BillingCycleForm extends Component {
     render() {
-        const { handleSubmit } = this.props            // --> const handleSubmit =  this.props.handleSubmit
+        const { handleSubmit, readOnly } = this.props            // --> const handleSubmit =  this.props.handleSubmit
         return (
             <div>
                 <form role='form' onSubmit={handleSubmit}>
                     <div className='box-body'>   
-                        <Field name='name' cols='12 4' placeholder='Preencha com o nome' component={Input} />
-                        <Field name='month' cols='12 4' placeholder='Preencha com o mês' type='number' component={Input} />
-                        <Field name='year' cols='12 4' placeholder='Preencha com o ano' type='number' component={Input} />
+                        <Field name='name' cols='12 4' placeholder='Preencha com o nome' readOnly={readOnly} component={Input} />
+                        <Field name='month' cols='12 4' placeholder='Preencha com o mês' type='number' readOnly={readOnly} component={Input} />
+                        <Field name='year' cols='12 4' placeholder='Preencha com o ano' type='number' readOnly={readOnly} component={Input} />
                     </div>
                     <div className='box-footer'>   
                         <button type='submit' className='btn btn-primary'>Submit</button>
