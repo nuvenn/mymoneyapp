45bd9f929305c566697397611b1c8cdbf85e6450
Daniel de Sales da Silva <ssales.daniel@gmail.com>
2018-08-24 14:44:45 -0300

    refatorei routes

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -6,14 +6,14 @@ import promise from 'redux-promise'
 import multi from 'redux-multi'
 import thunk from 'redux-thunk'
 
+import Routes from './main/routes'
 import reducers from './main/reducers'
-import App from './main/app'
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 const store = applyMiddleware(multi, thunk, promise)(createStore)(reducers, devTools)
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <Routes />
     </Provider>    
 , document.getElementById('app'))
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -4,14 +4,13 @@ import Header from '../common/template/header'
 import SideBar from '../common/template/sidebar'
 import Footer from '../common/template/footer'
 import Messages from '../common/msg/messages'
-import Routes from './routes'
  
 export default props => (
     <div className="wrapper">
         <Header />
         <SideBar />
         <div className="content-wrapper">
-            <Routes />
+            {props.children}
         </div>
         <Footer />
         <Messages />
