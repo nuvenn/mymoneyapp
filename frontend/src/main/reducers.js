13272045bab14a5e3d295df2d5e5a03e6f788dd4
Daniel de Sales da Silva <ssales.daniel@gmail.com>
2018-08-27 14:39:51 -0300

    adicionei auth

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -2,16 +2,18 @@ import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 import { reducer as toastrReducer } from 'react-redux-toastr'
 
-import DashboardReducer from '../dashboard/dashboardReducer'
-import TabReducer from '../common/tab/tabReducer'
+import dashboardReducer from '../dashboard/dashboardReducer'
+import tabReducer from '../common/tab/tabReducer'
 import billingCycleReducer from '../billingCycle/billingCycleReducer'
+import authReducer from '../auth/authReducer';
 
 const rootReducer = combineReducers({
-    dashboard: DashboardReducer,
-    tab: TabReducer,
+    dashboard: dashboardReducer,
+    tab: tabReducer,
     billingCycle: billingCycleReducer,
     form: formReducer,
-    toastr: toastrReducer 
+    toastr: toastrReducer,
+    auth: authReducer 
 })
 
 export default rootReducer
